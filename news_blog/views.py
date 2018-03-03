e25315773ea2d39afab978021993bda4788ca317
Nikitka2002 <ladunnikita02@gmail.com>
2018-03-03 18:45:25 +0600

    finish 2 task django

---
+++ news_blog/views.py
@@ -0,0 +1,19 @@
+from django.shortcuts import render
+
+# Create your views here.
+def index(request):
+    return render(request, 'news_blog/news_list.html', {'articles':articles})
+articles = [
+   {
+       'id': 1,
+       'title': 'First news',
+       'text': 'This is the worst first article'
+   },
+   {
+       'id': 2,
+       'title': 'Second news',
+       'text': 'This is the amazing second article'
+   }]
+
+def get_article(request, article_id):
+    return render(request, 'news_blog/news_article.html',{'article':articles[article_id - 1]})
